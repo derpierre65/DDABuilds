4f85a96918c5db82104485798a725db3809d0124
derpierre65 <git@derpierre65.de>
2025-11-04 23:09:50 +0000

    feat: add index page

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,9 +1,41 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    home
+  <q-page>
+    <div class="index-header-image" />
+
+    <div class="q-mx-auto tw:max-w-6xl q-gutter-y-md q-mt-md">
+      <h4>{{ $t('home.title') }}</h4>
+
+      <h6 class="text-justify">
+        {{ $t('home.line1') }}
+      </h6>
+
+      <p class="text-justify">
+        <i18next :translation="$t('home.line2')">
+          <template #link>
+            <a
+              href="https://dundefplanner.com/"
+              rel="noopener noreferrer"
+              target="_blank"
+              class="link"
+            >dundefplanner.com</a>
+          </template>
+        </i18next>
+      </p>
+    </div>
   </q-page>
 </template>
 
-<script setup lang="ts">
-import axios from 'axios';
-</script>
+<style lang="scss" scoped>
+.index-header-image {
+  background-image: url('/assets/images/index/1.jpg');
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  text-align: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: scroll;
+  height:300px;
+}
+</style>
